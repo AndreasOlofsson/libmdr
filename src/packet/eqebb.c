08d8acf75e4a33a76daae3141d9b8c46385ed793
Andreas Olofsson <ao@blazar.se>
2021-11-13 22:06:55 +0100

    Added missing license notices.

--- src/packet/eqebb.c
+++ src/packet/eqebb.c
@@ -1,3 +1,25 @@
+/*
+ * libmdr - MDR protocol library
+ *
+ *  Copyright (C) 2021 Andreas Olofsson
+ *
+ *
+ * This file is part of libmdr.
+ *
+ * libmdr is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libmdr is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libmdr. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include "mdr/packet/eqebb.h"
 
 #include <stdlib.h>
